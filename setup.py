2dda002d08c4c386e8af39358d53add8c1319e89
SamirMoustafa <samir.moustafa.97@gmail.com>
2025-08-18 14:33:25 +0200

    Add long description for pypi

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from os import path
 from setuptools import find_packages, setup
 
 __version__ = "0.3.5"
@@ -5,6 +6,10 @@ __version__ = "0.3.5"
 install_requires = [
     "torch>=1.13.1",
 ]
+
+# Get the long description from the README file
+with open(path.join(path.abspath(path.dirname(__file__)), "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 pkg_name = "torch_operation_counter"
 setup(
